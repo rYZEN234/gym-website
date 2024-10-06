c2a1511fd6f5be6080fb1b1b65914b68a77b502e
rYZEN234 <rshrivas373@gmail.com>
2024-10-06 11:18:13 +0530

    init

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        Designed And Developed By Alpha fitness gym.
+    </footer>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header>
+      <p>ALPHA FITNESS GYM</p>
+    </header>
+  );
+};
+
+export default Navbar;
